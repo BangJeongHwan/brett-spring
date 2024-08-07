8e97838b369fefa0aa72684ed4763c645066738a
bangjeonghwan <baju92@naver.com>
2024-08-07 22:36:51 +0900

    서블릿 요청 처리

--- src/main/java/com/kakaopaysec/brettspring/BrettspringApplication.java
+++ src/main/java/com/kakaopaysec/brettspring/BrettspringApplication.java
@@ -1,13 +1,33 @@
 package com.kakaopaysec.brettspring;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
 
 public class BrettspringApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();    // tomcat 말고도 사용 가능
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello "+name);
+                }
+            }).addMapping("/hello");    // servlet 추가할 때 container 가 어느 servlet 과 mapping 할지를 결정
+        });
         webServer.start();
     }
 }
