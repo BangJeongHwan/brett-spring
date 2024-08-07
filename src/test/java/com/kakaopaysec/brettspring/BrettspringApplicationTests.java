9808c7f9f7ac38e0484ef656ce4d96065ee77e6a
bangjeonghwan <baju92@naver.com>
2024-08-07 22:00:08 +0900

    init commit

---
+++ src/test/java/com/kakaopaysec/brettspring/BrettspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaopaysec.brettspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrettspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
