789d7da28c83c87d37516f0d358ccd0f4442d392
bangjeonghwan <baju92@naver.com>
2024-09-27 21:10:52 +0900

    자바코드 구성 정보 사용

--- src/main/java/com/kakaopaysec/brettspring/BrettspringApplication.java
+++ src/main/java/com/kakaopaysec/brettspring/BrettspringApplication.java
@@ -3,16 +3,30 @@ package com.kakaopaysec.brettspring;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class BrettspringApplication {
+
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
         // GenericWebApplicationContext 템플릿 메소드 패턴 : 상속을 통해 기능을 확장
         // onRefresh : 스프링 컨테이너를 초기화 하던 중에 부가적으로 작업을 수행할 때 사용
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        // AnnotationConfigApplicationContext : @Bean 을 읽어오기 위해서 변경
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             // 익명 클래스 사용
-
             @Override
             protected void onRefresh() {
                 super.onRefresh();  // 다른 초기화도 필요하기 때문에 냅둠
@@ -27,9 +41,7 @@ public class BrettspringApplication {
 
             }
         };
-        // 등록 순서는 중요하지 않다.
-        applicationContext.registerBean(HelloController.class); // spring container bean 등록
-        applicationContext.registerBean(SimpleHelloService.class); // spring container bean 등록
+        applicationContext.register(BrettspringApplication.class);  // 클래스를 등록시켜줌
         applicationContext.refresh();   // bean 초기화
     }
 }
